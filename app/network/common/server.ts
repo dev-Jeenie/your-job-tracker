139743b2fb7ddf70242eba284b3a1e5f3058c556
dev-Jeenie <devjeenie@gmail.com>
2024-10-02 17:39:16 +0900

    fix: fix backend endpoint

--- app/network/common/server.ts
+++ app/network/common/server.ts
@@ -3,7 +3,9 @@ import axios, { AxiosResponse } from "axios";
 export const serverResponseHandler = <T>(res: AxiosResponse<T>): T => res.data;
 
 export const serverInstance = axios.create({
-  baseURL: "http://localhost:3000/api/",
+  baseURL: process.env.NODE_ENV === "production" ?
+    "https://www.jobtrackers.co/api/":
+    "http://localhost:3000/api/",
   headers: {
     "Content-Type": "application/json",
   },
